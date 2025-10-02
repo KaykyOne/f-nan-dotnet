420fb8737ba536808edee5fef8ffb6fefbae7ec1
KaykyOne <kaykyzioti@gmail.com>
2025-10-01 23:56:16 -0300

    aaaaaaaaaaa

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,21 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using CrudNpN.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrudNpN.Models;
 
 public class Produto
 {
-    [Key]
     public int Id { get; set; }
-
     [Required, StringLength(120)]
     public string Nome { get; set; } = string.Empty;
-
+    [ModelBinder(BinderType = typeof(InvariantDecimalModelBinder))]
     [Range(0, 999999999)]
     public decimal Preco { get; set; }
-
     [Range(0, int.MaxValue)]
     public int Estoque { get; set; }
-
     public ICollection<VendaProduto>? Vendas { get; set; }
-
 }
